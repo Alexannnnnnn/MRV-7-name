8fb0827189a45b90d1e89f46203307e0ea2dbd30
Alexannnnnnn <49696034+Alexannnnnnn@users.noreply.github.com>
2019-05-25 17:59:57 -0500

    Add files via upload

---
+++ Assets/Elevate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Elevate : MonoBehaviour {
+  public float minHeight = 0f;
+  public float maxHeight = 2f;
+  public float velocity = 1;
+ 
+  void Update () {
+    float y = transform.position.y;
+    y += velocity * Time.deltaTime;
+    if (y > maxHeight) {
+    y = maxHeight;
+      velocity = -velocity;
+    }
+    if (y < minHeight) {
+      y = minHeight;
+      velocity = -velocity;
+    }
+    transform.position = new Vector3 (transform.position.x, y,  
+      transform.position.z);
+  }
+}
